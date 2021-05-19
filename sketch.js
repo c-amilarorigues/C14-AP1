7de6b965da5ce140dd9b8c30289c0e5650b17143
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-05-19 15:39:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var Student = {
 function setup() {
   
   createCanvas(400, 400);
-  //changing the roll no
+  //mudandoo o roll no (número do roll)
   console.log(Student.roll_no);
   Student.roll_no = 45;
   console.log(Student.roll_no);  
